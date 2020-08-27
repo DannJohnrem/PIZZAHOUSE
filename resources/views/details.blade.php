60afc25455e4f9f837c9551a39d0884e0f846f75
DannJohnrem <jonrhem10@gmail.com>
2020-08-27 16:18:33 +0800

    newly added commits

---
+++ resources/views/details.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-d-md">
+                Pizzas List - {{$id}}
+            </div>
+        </div>
+    </div>
+@endsection
+
